3c43bad04a1732a1c4cdec925f07b38d2dd9e198
Luke Seers <16389426+LukeSeers@users.noreply.github.com>
2022-02-03 09:56:30 +0100

    Fix #16 prevent connecting to yourself(SteamId)

--- FizzyFacepunch.cs
+++ FizzyFacepunch.cs
@@ -103,6 +103,12 @@ namespace Mirror.FizzySteam
         OnClientDisconnected.Invoke();
         return;
       }
+      
+      if (address == SteamUserID.ToString())
+      {
+        Debug.Log("You can't connect to yourself.");
+        return;
+      }
 
       FetchSteamID();
 
